f628a6ae610768ee113acb787ef413a0eba80f9d
Yeonji Choi <duswl8936@naver.com>
2023-05-17 17:54:30 +0900

    파일 업로드 css, 로직 구현 중

--- views/app.js
+++ views/app.js
@@ -1,61 +1,28 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const dotenv = require('dotenv');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-  name: 'session-cookie',
-}));
-
-// multer 미들웨어 사용
-const multer = require('multer');
-
-
-const upload = multer({
-    storage: multer.diskStorage({
-      destination(req, file, done) {
-        done(null, 'uploads/');
-      },
-      filename(req, file, done) {
-        const ext = path.extname(file.originalname);
-        done(null, path.basename(file.originalname, ext)+ ext);
-      },
-    }),
-    limits: { fileSize: 5 * 1024 * 1024 },
-});
-
-const multer = require('multer');
-const fs = require('fs');
-
-
-try {
-  fs.readdirSync('uploads');
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync('uploads');
-}
-router.get('/', (req , res)=>{
-    res.sendFile(path.join(__dirname, 'multipart.html'));
-})
-router.post('/', upload.single('chooseFile'), (req, res) => {
-    console.log(req.file);
-    res.send('ok');
-})
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
