f39f61ac6e7aba72d76f67cd9a4056022148d708
Yeonji Choi <duswl8936@naver.com>
2023-05-22 13:34:50 +0900

    feat : 한글 깨짐 현상 코드 추가

--- routes/chartRouter.js
+++ routes/chartRouter.js
@@ -16,6 +16,9 @@ let storage = multer.diskStorage({
     cb(null, path.resolve(__dirname, "../uploads")); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
   },
   filename: function (req, file, cb) {
+    file.originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    ); //한글 깨짐 현상 처리
     cb(null, file.originalname); // cb 콜백함수를 통해 전송된 파일 이름 설정
   },
 });
@@ -24,7 +27,6 @@ const upload = multer({ storage: storage });
 //데이터 보내기
 router.post("/", upload.single("txtFile"), async (req, res, next) => {
   let fileName = req.file.originalname;
-  res.setCharacterEncoding("UTF-8");
   console.log(`파일 이름 확인용 : ${fileName}`);
 
   let arr;
